9462d2cebdf7366871b0fd967309078853a11db1
Matheus Rodrigues Santos <98832555+santos95mat@users.noreply.github.com>
2022-02-01 15:23:04 -0300

    Update TP.js

--- TP.js
+++ TP.js
@@ -6,25 +6,24 @@ function codificador(){
 	let t = ["t","e","n","i","s","p","o","l","a","r"];
 	let p = ["p","o","l","a","r","t","e","n","i","s"];
 	let fraseCripto = "";
-
-  for(f=0; f<frase.length; f++){
-    for(i=0; i<t.length; i++){
-      if(frase[f] === t[i]){
-        fraseCripto += p[i];
-      }
-    }
-    if (fraseCripto.length != f+1){
-      fraseCripto += frase[f]
-    }
-  }
-    
+	
+	for(f=0; f<frase.length; f++){
+		for(i=0; i<t.length; i++){
+			if(frase[f] === t[i]){
+				fraseCripto += p[i];
+			}
+		}
+		if (fraseCripto.length != f+1){
+			fraseCripto += frase[f]
+		}
+	}
 	console.log(`Frase codificada: ${fraseCripto}`);
 	console.log(" ");
-  const q = prompt("deseia fechar o programa? [s] ou [n] ")
-  if(q === "n") { 
-	  console.clear();
-	  codificador() 
-  }
-
+	const q = prompt("deseia fechar o programa? [s] ou [n] ")
+	if(q === "n") { 
+		console.clear();
+		codificador() 
+	}
 }
+
 codificador()
